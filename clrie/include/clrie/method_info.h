cd7ba33ad3bfd189b3a81a7f7a702b5a7507d56a
Rafał Rzepecki <rafal@app.land>
2021-04-18 22:41:30 +0200

    Correctly stringify ref struct values
    Ref struct values cannot be boxed, so previous stringification code
    was invalid for these. Putting structs in a local and calling
    constrained callvirt on the local address handles this case; it's
    now used for all structs except for Nullable<T>, which requires
    special handling to correctly return null values as null.

--- clrie/include/clrie/method_info.h
+++ clrie/include/clrie/method_info.h
@@ -15,6 +15,12 @@ constexpr GUID com::guid_of<ITokenType>() noexcept {
     return "77655B33-1B29-4285-9F2D-FF9526E3A0AA"_guid;
 }
 
+template<>
+constexpr GUID com::guid_of<ICompositeType>() noexcept {
+    using namespace com::literals;
+    return "06B9FD79-0386-4CF3-93DD-A23E95EBC225"_guid;
+}
+
 namespace clrie {
     struct method_info : public com::ptr<IMethodInfo> {
         method_info(IMethodInfo *info = nullptr) noexcept : com::ptr<IMethodInfo>(info) {}
