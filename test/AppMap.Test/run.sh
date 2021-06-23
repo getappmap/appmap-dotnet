6942a0b4d62523b6bc65645ee09dad753fb5bd80
Rafał Rzepecki <rafal@app.land>
2021-06-23 20:47:17 +0200

    Show full diff in integration tests

--- test/AppMap.Test/run.sh
+++ test/AppMap.Test/run.sh
@@ -5,6 +5,6 @@ cd `dirname $0`
 export APPMAP_OUTPUT_DIR=`mktemp -td appmap.test.XXX`
 dotnet appmap test -c Release
 
-diff -ru expected $APPMAP_OUTPUT_DIR
+diff -urN expected $APPMAP_OUTPUT_DIR
 
 rm -rf $APPMAP_OUTPUT_DIR
