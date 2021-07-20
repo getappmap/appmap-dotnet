b0c9e7e4bf2b345ffc992c2c62f8ea7c4b2bb4ce
Rafał Rzepecki <rafal@app.land>
2021-07-20 19:37:01 +0200

    Fix docker build scripts

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,8 @@
-FROM proddiagbuild.azurecr.io/clrie-build-ubuntu:latest AS clrie
-ARG CLRIE_VERSION=1.0.36
-RUN curl -L https://github.com/microsoft/CLRInstrumentationEngine/archive/instrumentationengine-release-${CLRIE_VERSION}.tar.gz | tar zxv
-RUN cd CLRInstrumentationEngine-instrumentationengine-release-${CLRIE_VERSION}/src; ./build.sh
-
 FROM ubuntu
 RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ git && rm -rf /var/lib/apt/lists/*
-ARG CLRIE_VERSION=1.0.36
-COPY --from=clrie CLRInstrumentationEngine-instrumentationengine-release-${CLRIE_VERSION}/out/Linux/bin/x64.Debug/ClrInstrumentationEngine/libInstrumentationEngine.so /out/
+ENV CPM_SOURCE_CACHE=/var/cache/cpm
 COPY cmake /src/cmake
 COPY CMakeLists.txt /src
 RUN cd /src; cmake -Bbuild -DCMAKE_BUILD_TYPE=Release || :
 COPY . /src
-RUN cd /src; cmake -Bbuild -DCMAKE_BUILD_TYPE=Release; cmake --build build -j4; cp build/*.so config/* /out/
+RUN mkdir /out; cd /src; cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=false; cmake --build build -j4; cp build/*.so /out/
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,11 +1,34 @@
 #/bin/bash -e
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
+mkdir -p $BASEDIR/out
 
-docker build ${BASEDIR}
+CLRIE_VERSION=1.0.36
+CLRIE_TARBALL=instrumentationengine-release-${CLRIE_VERSION}.tar.gz
+CLRIE_DIR=CLRInstrumentationEngine-instrumentationengine-release-${CLRIE_VERSION}
 
-mkdir -p out
-image=$(docker build -q $BASEDIR)
-id=$(docker create $image)
-docker cp $id:/out - | tar xvC $BASEDIR
-docker rm -v $id > /dev/null
+build_clrie() {
+  version="$1"
+  if ! [ -d $CLRIE_DIR ]; then
+    curl -L https://github.com/microsoft/CLRInstrumentationEngine/archive/$CLRIE_TARBALL | tar zxv
+  fi
+
+  pushd $CLRIE_DIR
+  docker build -t clrie-build-ubuntu src/unix/docker/dockerfiles/build/ubuntu/
+  sh src/unix/docker/docker-build.sh clrie-build-ubuntu x64 Release
+  cp out/Linux/bin/x64.Release/ClrInstrumentationEngine/* $BASEDIR/out/
+}
+
+build_appmap() {
+  docker build ${BASEDIR}
+
+  image=$(docker build -q $BASEDIR)
+  id=$(docker create $image)
+  docker cp "$id:/src/build/libappmap-instrumentation.so" $BASEDIR/out
+  docker rm -v $id > /dev/null
+}
+
+[ -f $BASEDIR/out/libInstrumentationEngine.so ] || build_clrie
+build_appmap
+cp config/* out
+dotnet pack -c release launcher
