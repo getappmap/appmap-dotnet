0da25e50e4c11c701d1e81d07ac53dcaf48500f1
Rafał Rzepecki <rafal@app.land>
2021-08-07 15:30:09 +0200

    More robust thread interleave test

--- test/AppMap.Test/Threads.cs
+++ test/AppMap.Test/Threads.cs
@@ -6,8 +6,15 @@ namespace AppMap.Test
 {
     namespace Code {
         public class Threads {
-            static public void Fun1() { Thread.Sleep(100); }
-            static public void Fun2() { Thread.Sleep(100); }
+            public static SemaphoreSlim sem1;
+            public static SemaphoreSlim sem2;
+            static public void Fun1() {
+                sem1.Wait();
+            }
+            static public void Fun2() {
+                sem1.Release();
+                sem2.Wait();
+            }
         }
     }
 
@@ -15,14 +22,17 @@ namespace AppMap.Test
         [Fact]
         public void CanInterleave()
         {
+            Code.Threads.sem1 = new SemaphoreSlim(0);
+            Code.Threads.sem2 = new SemaphoreSlim(0);
+
             var t1 = new Thread(new ThreadStart(Code.Threads.Fun1));
             var t2 = new Thread(new ThreadStart(Code.Threads.Fun2));
 
             t1.Start();
-            Thread.Sleep(10);
             t2.Start();
 
             t1.Join();
+            Code.Threads.sem2.Release();
             t2.Join();
         }
     }
